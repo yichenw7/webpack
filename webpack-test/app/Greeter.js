0f941942f87fedfe7e47e08a64a7e542256ea104
yichenw7 <yichenw7@126.com>
2017-07-10 18:34:23 +0800

    webpack进阶

---
+++ webpack-test/app/Greeter.js
@@ -0,0 +1,18 @@
+// Greeter.js
+
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = "Hi there and greetings!";
+//     return greet;
+// };
+
+
+// var config = require('./config.json');
+//
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = config.greetText;
+//     return greet;
+// };
+
+//Greeter,js
---
+++ webpack-test/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+
+        // 注：“__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
+    },
+    module: {//在配置文件里添加JSON loader
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: "json"
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',//在webpack的module部分的loaders里进行配置即可
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+
+        ]
+    },
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        colors: true,//终端中输出结果为彩色
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    }
+}
